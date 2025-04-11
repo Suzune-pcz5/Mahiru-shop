6ae44422600e35a3233232d14ac676868a748165
Hiển Giảng <hiengiang353@gmail.com>
2025-04-11 22:34:04 +0700

    Update index_account.php

--- Mahiru-shop/index_account.php
+++ Mahiru-shop/index_account.php
@@ -224,13 +224,13 @@ $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
                         </select>
                     </div>
                     <div class="filter-price">
-                        <h3>Price:</h3>
-                        <div class="price-range-inputs">
-                            <input type="number" name="min_price" min="0" max="300" placeholder="Min" value="...">
-                            <span>to</span>
-                            <input type="number" name="max_price" min="0" max="300" placeholder="Max" value="...">
-                        </div>
+                    <h3>Price:</h3>
+                    <div class="price-range-inputs">
+                        <input type="number" name="min_price" min="0" max="300" placeholder="Min" value="<?php echo htmlspecialchars($minPrice); ?>">
+                        <span>to</span>
+                        <input type="number" name="max_price" min="0" max="300" placeholder="Max" value="<?php echo htmlspecialchars($maxPrice); ?>">
                     </div>
+                </div>
                     <button type="submit" class="filter-button" style="margin-top: 10px">Search</button>
                 </form>
             </div>
