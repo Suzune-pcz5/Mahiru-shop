95e2ebcbf27ae39cd01f08ad76afd1d8edac7917
Suzune-pcz5 <fireflypcz5@gmail.com>
2025-04-16 15:23:27 +0700

    Update order_confirmation.php

--- Mahiru-shop/order_confirmation.php
+++ Mahiru-shop/order_confirmation.php
@@ -73,7 +73,6 @@ unset($_SESSION['order_id']);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Order Confirmation - Mahiru Shop</title>
     <link rel="stylesheet" href="./css/styles.css">
-    <link rel="stylesheet" href="./css/categories.css">
     <link rel="stylesheet" href="./css/checkout.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <style>
